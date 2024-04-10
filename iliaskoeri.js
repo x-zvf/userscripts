701665df3f242c7eb12ea37604c52bad4246c3f6
Péter Bohner (xzvf) <peter@bohner.me>
2024-04-10 09:00:50 +0200

    Fixed iliaskoeri navigation to dashboard for ILIAS v8.10

--- iliaskoeri.js
+++ iliaskoeri.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Ilias[kœri]
-// @version      1.1
+// @version      1.2
 // @description  [kœri] makes everything better; Automate the login process to KIT's ilias.
 // @author       xzvf (Péter Bohner)
 // @match        https://ilias.studium.kit.edu/*
@@ -20,8 +20,8 @@ let iliasUrl = /https\:\/\/ilias\.studium\.kit\.edu\/.*/;
 let shiboUrl = /https\:\/\/idp\.scc\.kit\.edu\/idp\/profile\/SAML2\/.*/;
 
 function login() {
-    if(shiboUrl.test(location.href)) {
-        if(!GM_getValue("shibLoginAttempted", false)) {
+    if (shiboUrl.test(location.href)) {
+        if (!GM_getValue("shibLoginAttempted", false)) {
             GM_setValue("shibLoginAttempted", true);
             document.getElementById("sbmt").dispatchEvent(new MouseEvent("click"));
         }
@@ -30,26 +30,26 @@ function login() {
 
 (function() {
     'use strict';
-    if(iliasUrl.test(location.href)) {
+    if (iliasUrl.test(location.href)) {
         var loginAttempted = GM_getValue("shibLoginAttempted", false);
         GM_setValue("shibLoginAttempted", false);
-        if(loginAttempted) {
-           var dashboardLinkImg = Array.from(document.getElementsByTagName("img")).filter(x => /Dashboard/.test(x.getAttribute("alt")))[0]
-           if(dashboardLinkImg) {
-               dashboardLinkImg.parentElement.dispatchEvent(new MouseEvent("click"));
-           }
+        if (loginAttempted) {
+            var dashboardLink = Array.from(document.querySelectorAll(".il-link[role=menuitem]")).filter(x => /jumpToMemberships/.test(x.getAttribute("href")))[0]
+            if (dashboardLink) {
+                dashboardLink.dispatchEvent(new MouseEvent("click"));
+            }
         }
 
         var loginButton = Array.from(document.getElementsByTagName("a")).filter(x => /login.php\?.*/.test(x.getAttribute("href")))[0]
-        if(loginButton && !/login.php\?.*/.test(location.href)) {
+        if (loginButton && !/login.php\?.*/.test(location.href)) {
             loginButton.dispatchEvent(new MouseEvent("click"));
         }
         var shibButton = document.getElementById("button_shib_login")
-        if(shibButton) {
+        if (shibButton) {
             shibButton.dispatchEvent(new MouseEvent("click"));
         }
-    } else if(shiboUrl.test(location.href)) {
-        if(shouldAutofill) {
+    } else if (shiboUrl.test(location.href)) {
+        if (shouldAutofill) {
             document.getElementById("name").value = username;
             document.getElementById("password").value = password;
             login();
@@ -58,3 +58,7 @@ function login() {
         }
     }
 })();
+
+
+
+
